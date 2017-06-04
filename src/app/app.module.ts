1e1a00e07374a5002bc626fe077918bbef4130dc
Erik Dahlström <erik.e.dahlstrom@gmail.com>
2017-06-04 21:20:31 +0200

    removed some unused files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent }  from './app.component';
-import { PersonDetailComponent } from './person-detail.component';
 import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpModule ],
-  declarations: [ AppComponent,
-  PersonDetailComponent ],
+  declarations: [ AppComponent, ],
   bootstrap:    [ AppComponent, ]
 })
 export class AppModule { }
